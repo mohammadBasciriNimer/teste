9d3c20283dfbbd27fb84d4cc99d9f4d16272b69f
Mohammad Basciri Nimer Hammad <mohammad@riosulense.com.br>
2025-11-21 09:47:14 -0300

    Primeiro commit

--- interfaces/screens/users/login.py
+++ interfaces/screens/users/login.py
@@ -1,30 +1,30 @@
-from pathlib import Path
-import streamlit as st
-from src.infra.auth.user import AuthUser
-from src.settings.paths import SettingsPath
+# from pathlib import Path
+# import streamlit as st
+# from src.infra.auth.user import AuthUser
+# from src.settings.paths import SettingsPath
 
 
 
-def show_page_login():
-    st.set_page_config(page_title="House Rio",page_icon=SettingsPath.ASSETS_PATH /'rio_circulo.png',layout='centered')
-    page_col = st.columns([3,1])
+# def show_page_login():
+    # st.set_page_config(page_title="House Rio",page_icon=SettingsPath.ASSETS_PATH /'rio_circulo.png',layout='centered')
+    # page_col = st.columns([3,1])
 
-    st.image(image=SettingsPath.ASSETS_PATH /'image_login.png')
+    # st.image(image=SettingsPath.ASSETS_PATH /'image_login.png')
 
-    with st.form('form_login',height='stretch') as form:
-        st.subheader('Login',divider=True)
-        username = st.text_input("Usuário",placeholder='Usuario Rio')
-        password = st.text_input("Senha",placeholder='Senha Rio',type="password")
-        submit_button = st.form_submit_button("Acessar","Acessar sistema",width='stretch',type='secondary')
+    # with st.form('form_login',height='stretch') as form:
+        # st.subheader('Login',divider=True)
+        # username = st.text_input("Usuário",placeholder='Usuario Rio')
+        # password = st.text_input("Senha",placeholder='Senha Rio',type="password")
+        # submit_button = st.form_submit_button("Acessar","Acessar sistema",width='stretch',type='secondary')
 
-        if submit_button:
-            if not username or not password:
-                st.error('Faltou preencher algum campo')
-            else:
-                user_valid, group_user = AuthUser.user_valid(username,password)
-                if user_valid:
-                    st.session_state["authenticated"] = True
-                    st.session_state["username"] = username
-                    st.session_state["group"] = group_user                    
-                else:
-                    st.error('Usuário ou senha inválido!')
+        # if submit_button:
+            # if not username or not password:
+                # st.error('Faltou preencher algum campo')
+            # else:
+                # user_valid, group_user = AuthUser.user_valid(username,password)
+                # if user_valid:
+                    # st.session_state["authenticated"] = True
+                    # st.session_state["username"] = username
+                    # st.session_state["group"] = group_user                    
+                # else:
+                    # st.error('Usuário ou senha inválido!')
